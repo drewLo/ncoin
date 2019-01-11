c045de98e31c522aef6c8f531a87de346eafe697
Drew Lough <drew.lough@gmail.com>
2019-01-10 17:31:35 -0800

    add imports and instances

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
-import { Block, generateNextBlock, getBlockchain} from './blockchain';
+import {Block, generateNextBlock, getBlockchain} from './blockchain';
+import {connectToPers, getSocket, initP2PServer} from './p2p';
+
+const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
+const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
 
 const initHttpServer = (myHttpPort: number) => {
     const app = express();
