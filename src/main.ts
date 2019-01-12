e0bbb3216272a4fd87fc241823a6dc53ab087bda
Drew Lough <drew.lough@gmail.com>
2019-01-12 15:30:03 -0800

    typo fixes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
 import {Block, generateNextBlock, getBlockchain} from './blockchain';
-import {connectToPers, getSocket, initP2PServer} from './p2p';
+import {connectToPeers, getSockets, initP2PServer} from './p2p';
 
 const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
 const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
@@ -18,16 +18,16 @@ const initHttpServer = (myHttpPort: number) => {
         const newBlock: Block = generateNextBlock(req.body.data);
         res.send(newBlock);
     });
-    app.get('/pers', (req, res) => {
-        res.snd(getSockets().map(( s: any ) => s._socket.remotAddress+ ':' + s._socket.remotePort));
+    app.get('/peers', (req, res) => {
+        res.send(getSockets().map(( s: any ) => s._socket.remoteAddress+ ':' + s._socket.remotePort));
     });
     app.post('/addPeer', (req, res) => {
-        connectToPers(req.body.peer);
+        connectToPeers(req.body.peer);
         res.send();
     });
 
     app.listen(myHttpPort, () => {
-        console.log('Listening ttp on port: ' + myHttpPort);
+        console.log('Listening http on port: ' + myHttpPort);
     });
 };
 
