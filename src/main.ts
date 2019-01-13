59f6ce46409947d891df186f73a5fff7e4a7fcf5
Drew Lough <drew.lough@gmail.com>
2019-01-13 15:36:09 -0800

    formatting

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 
 import { Block, generateNextBlock, getBlockchain } from './blockchain';
+import { connectToPeers, getSockets, initP2PServer } from './p2p';
 
 const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
 const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
