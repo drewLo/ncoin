a47161f6bff28ed78e90bf4ce1abc708d38b3b0c
Drew Lough <drew.lough@gmail.com>
2019-01-13 15:35:03 -0800

    add difficulty getters

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -32,6 +32,27 @@ const getBlockchain = (): Block[] => blockchain;
 
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
+const getDifficulty = (aBlockchain: Block[]): number => {
+        const latestBlock: Block = aBlockchain[blockchain.length - 1];
+        if (latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 && latestBlock.index !== 0) {
+                return getAdjustedDifficulty(latestBlock, aBlockchain);
+        } else {
+                return latestBlock.difficulty;
+        }
+};
+
+const getAdjustedDifficulty = (latestBlock: Block, aBlockchain: Block[]) => {
+        const prevAdjustmentBlock: Block = aBlockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+        const timeExpected: number = BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+        const timeTaken: number = latestBlock.timestamp - prevAdjustmentBlock.timestamp;
+        if (timeTaken < timeExpected / 2) {
+                return prevAdjustmentBlock.difficulty + 1;
+        } else if (timeTaken > timeExpected * 2) {
+                return prevAdjustmentBlock.difficulty - 1;
+        } else {
+                return prevAdjustmentBlock.difficulty;
+        }
+};
 
 const getCurrentTimestamp = (): number => Math.round(new Date().getTime() / 1000);
 
