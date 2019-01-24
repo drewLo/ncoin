5beaa8fc5cbe60e9fad5e4010339b4c7f0f55f8f
Drew Lough <drew.lough@gmail.com>
2019-01-23 22:08:11 -0800

    dummy commits

--- src/blockchain.ts
+++ src/blockchain.ts
@@ -3,41 +3,41 @@ import { broadcastLatest } from "./p2p";
 import { hexToBinary } from "./util";
 import { stringify } from "querystring";
 class Block {
-        public index: number;
-        public hash: string;
-        public previousHash: string;
-        public timestamp: number;
-        public data: string;
-        public difficulty: number;
-        public nonce: number;
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+    public difficulty: number;
+    public nonce: number;
 
-        constructor(
-                index: number,
-                hash: string,
-                previousHash: string,
-                timestamp: number,
-                data: string,
-                difficulty: number,
-                nonce: number
-        ) {
-                this.index = index;
-                this.previousHash = previousHash;
-                this.timestamp = timestamp;
-                this.data = data;
-                this.hash = hash;
-                this.difficulty = difficulty;
-                this.nonce = nonce;
-        }
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        timestamp: number,
+        data: string,
+        difficulty: number,
+        nonce: number
+    ) {
+        this.index = index;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = hash;
+        this.difficulty = difficulty;
+        this.nonce = nonce;
+    }
 }
 
 const genesisBlock: Block = new Block(
-        0,
-        "91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627",
-        "",
-        1465154705,
-        "my genesis block!!",
-        0,
-        0
+    0,
+    "91a73664bc84c0baa1fc75ea6e4aa6d1d20c5df664c724e3159aefc2e1186627",
+    "",
+    1465154705,
+    "my genesis block!!",
+    0,
+    0
 );
 
 let blockchain: Block[] = [genesisBlock];
@@ -53,238 +53,238 @@ const BLOCK_GENERATION_INTERVAL: number = 10;
 const DIFFICULTY_ADJUSTMENT_INTERVAL: number = 10;
 
 const getDifficulty = (aBlockchain: Block[]): number => {
-        const latestBlock: Block = aBlockchain[blockchain.length - 1];
-        if (
-                latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 &&
-                latestBlock.index !== 0
-        ) {
-                return getAdjustedDifficulty(latestBlock, aBlockchain);
-        } else {
-                return latestBlock.difficulty;
-        }
+    const latestBlock: Block = aBlockchain[blockchain.length - 1];
+    if (
+        latestBlock.index % DIFFICULTY_ADJUSTMENT_INTERVAL === 0 &&
+        latestBlock.index !== 0
+    ) {
+        return getAdjustedDifficulty(latestBlock, aBlockchain);
+    } else {
+        return latestBlock.difficulty;
+    }
 };
 
 const getAdjustedDifficulty = (latestBlock: Block, aBlockchain: Block[]) => {
-        const prevAdjustmentBlock: Block =
-                aBlockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
-        const timeExpected: number =
-                BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
-        const timeTaken: number =
-                latestBlock.timestamp - prevAdjustmentBlock.timestamp;
-        if (timeTaken < timeExpected / 2) {
-                return prevAdjustmentBlock.difficulty + 1;
-        } else if (timeTaken > timeExpected * 2) {
-                return prevAdjustmentBlock.difficulty - 1;
-        } else {
-                return prevAdjustmentBlock.difficulty;
-        }
+    const prevAdjustmentBlock: Block =
+        aBlockchain[blockchain.length - DIFFICULTY_ADJUSTMENT_INTERVAL];
+    const timeExpected: number =
+        BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
+    const timeTaken: number =
+        latestBlock.timestamp - prevAdjustmentBlock.timestamp;
+    if (timeTaken < timeExpected / 2) {
+        return prevAdjustmentBlock.difficulty + 1;
+    } else if (timeTaken > timeExpected * 2) {
+        return prevAdjustmentBlock.difficulty - 1;
+    } else {
+        return prevAdjustmentBlock.difficulty;
+    }
 };
 
 const getCurrentTimestamp = (): number =>
-        Math.round(new Date().getTime() / 1000);
+    Math.round(new Date().getTime() / 1000);
 
 const generateNextBlock = (blockData: string) => {
-        const previousBlock: Block = getLatestBlock();
-        const difficulty: number = getDifficulty(getBlockchain());
-        console.log("difficulty: " + difficulty);
-        const nextIndex: number = previousBlock.index + 1;
-        const nextTimestamp: number = getCurrentTimestamp();
-        const newBlock: Block = findBlock(
-                nextIndex,
-                previousBlock.hash,
-                nextTimestamp,
-                blockData,
-                difficulty
-        );
-        addBlockToChain(newBlock);
-        broadcastLatest();
-        return newBlock;
+    const previousBlock: Block = getLatestBlock();
+    const difficulty: number = getDifficulty(getBlockchain());
+    console.log("difficulty: " + difficulty);
+    const nextIndex: number = previousBlock.index + 1;
+    const nextTimestamp: number = getCurrentTimestamp();
+    const newBlock: Block = findBlock(
+        nextIndex,
+        previousBlock.hash,
+        nextTimestamp,
+        blockData,
+        difficulty
+    );
+    addBlockToChain(newBlock);
+    broadcastLatest();
+    return newBlock;
 };
 
 const findBlock = (
-        index: number,
-        previousHash: string,
-        timestamp: number,
-        data: string,
-        difficulty: number
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string,
+    difficulty: number
 ): Block => {
-        let nonce = 0;
-        while (true) {
-                const hash: string = calculateHash(
-                        index,
-                        previousHash,
-                        timestamp,
-                        data,
-                        difficulty,
-                        nonce
-                );
-                if (hashMatchesDifficulty(hash, difficulty)) {
-                        return new Block(
-                                index,
-                                hash,
-                                previousHash,
-                                timestamp,
-                                data,
-                                difficulty,
-                                nonce
-                        );
-                }
-                nonce++;
+    let nonce = 0;
+    while (true) {
+        const hash: string = calculateHash(
+            index,
+            previousHash,
+            timestamp,
+            data,
+            difficulty,
+            nonce
+        );
+        if (hashMatchesDifficulty(hash, difficulty)) {
+            return new Block(
+                index,
+                hash,
+                previousHash,
+                timestamp,
+                data,
+                difficulty,
+                nonce
+            );
         }
+        nonce++;
+    }
 };
 
 const calculateHash = (
-        index: number,
-        previousHash: string,
-        timestamp: number,
-        data: string,
-        difficulty: number,
-        nonce: number
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string,
+    difficulty: number,
+    nonce: number
 ): string =>
-        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
 const calculateHashForBlock = (block: Block): string =>
-        calculateHash(
-                block.index,
-                block.previousHash,
-                block.timestamp,
-                block.data,
-                block.difficulty,
-                block.nonce
-        );
+    calculateHash(
+        block.index,
+        block.previousHash,
+        block.timestamp,
+        block.data,
+        block.difficulty,
+        block.nonce
+    );
 
 const addBlock = (newBlock: Block) => {
-        if (isValidnewBlock(newBlock, getLatestBlock())) {
-                blockchain.push(newBlock);
-        }
+    if (isValidnewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+    }
 };
 
 const isValidBlockStructure = (block: Block): boolean => {
-        return (
-                typeof block.index === "number" &&
-                typeof block.hash === "string" &&
-                typeof block.previousHash === "string" &&
-                typeof block.timestamp === "number" &&
-                typeof block.data === "string"
-        );
+    return (
+        typeof block.index === "number" &&
+        typeof block.hash === "string" &&
+        typeof block.previousHash === "string" &&
+        typeof block.timestamp === "number" &&
+        typeof block.data === "string"
+    );
 };
 
 const isValidnewBlock = (newBlock: Block, previousBlock: Block): boolean => {
-        if (previousBlock.index + 1 !== newBlock.index) {
-                console.log("invalid index");
-                return false;
-        } else if (previousBlock.hash !== newBlock.previousHash) {
-                console.log("invalid previous hash");
-                return false;
-        } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
-                console.log(
+    if (previousBlock.index + 1 !== newBlock.index) {
+        console.log("invalid index");
+        return false;
+    } else if (previousBlock.hash !== newBlock.previousHash) {
+        console.log("invalid previous hash");
+        return false;
+    } else if (calculateHashForBlock(newBlock) !== newBlock.hash) {
+        console.log(
             typeof newBlock.hash + " " + typeof calculateHashForBlock(newBlock)
-                );
-                console.log(
-                        "invalid hash: " +
-                                calculateHashForBlock(newBlock) +
-                                " " +
-                                newBlock.hash
-                );
-                return false;
-        }
-        return true;
+        );
+        console.log(
+            "invalid hash: " +
+                calculateHashForBlock(newBlock) +
+                " " +
+                newBlock.hash
+        );
+        return false;
+    }
+    return true;
 };
 
 const getAccumulatedDifficulty = (aBlockchain: Block[]): number => {
-        return aBlockchain
-                .map(block => block.difficulty)
-                .map(difficulty => Math.pow(2, difficulty))
-                .reduce((a, b) => a + b);
+    return aBlockchain
+        .map(block => block.difficulty)
+        .map(difficulty => Math.pow(2, difficulty))
+        .reduce((a, b) => a + b);
 };
 
 const isValidTimeStamp = (newBlock: Block, previousBlock: Block): boolean => {
-        return (
-                previousBlock.timestamp - 60 < newBlock.timestamp &&
-                newBlock.timestamp - 60 < getCurrentTimestamp()
-        );
+    return (
+        previousBlock.timestamp - 60 < newBlock.timestamp &&
+        newBlock.timestamp - 60 < getCurrentTimestamp()
+    );
 };
 
 const hasValidHash = (block: Block): boolean => {
-        if (!hashMatchesBlockContent(block)) {
-                console.log("invalid hash, got: " + block.hash);
-                return false;
-        }
-        if (!hashMatchesDifficulty(block.hash, block.difficulty)) {
-                console.log(
-                        "block difficulty not satisfied. Expected: " +
-                                block.difficulty +
-                                "got: " +
-                                block.hash
-                );
-                return false;
-        }
-        return true;
+    if (!hashMatchesBlockContent(block)) {
+        console.log("invalid hash, got: " + block.hash);
+        return false;
+    }
+    if (!hashMatchesDifficulty(block.hash, block.difficulty)) {
+        console.log(
+            "block difficulty not satisfied. Expected: " +
+                block.difficulty +
+                "got: " +
+                block.hash
+        );
+        return false;
+    }
+    return true;
 };
 
 const hashMatchesBlockContent = (block: Block): boolean => {
-        const blockHash: string = calculateHashForBlock(block);
-        return blockHash === block.hash;
+    const blockHash: string = calculateHashForBlock(block);
+    return blockHash === block.hash;
 };
 
 const hashMatchesDifficulty = (hash: string, difficulty: number): boolean => {
-        const hashInBinary: string = hexToBinary(hash);
-        const requiredPrefix: string = "0".repeat(difficulty);
-        return hashInBinary.startsWith(requiredPrefix);
+    const hashInBinary: string = hexToBinary(hash);
+    const requiredPrefix: string = "0".repeat(difficulty);
+    return hashInBinary.startsWith(requiredPrefix);
 };
 
 const isValidChain = (blockchainToValidate: Block[]): boolean => {
-        const isValidGenesis = (block: Block): boolean => {
-                return JSON.stringify(block) === JSON.stringify(genesisBlock);
-        };
+    const isValidGenesis = (block: Block): boolean => {
+        return JSON.stringify(block) === JSON.stringify(genesisBlock);
+    };
 
-        if (!isValidGenesis(blockchainToValidate[0])) {
-                return false;
-        }
+    if (!isValidGenesis(blockchainToValidate[0])) {
+        return false;
+    }
 
-        for (let i = 1; i < blockchainToValidate.length; i++) {
-                if (
-                        !isValidnewBlock(
-                                blockchainToValidate[i],
-                                blockchainToValidate[i - 1]
-                        )
-                ) {
-                        return false;
-                }
+    for (let i = 1; i < blockchainToValidate.length; i++) {
+        if (
+            !isValidnewBlock(
+                blockchainToValidate[i],
+                blockchainToValidate[i - 1]
+            )
+        ) {
+            return false;
         }
-        return true;
+    }
+    return true;
 };
 
 const addBlockToChain = (newBlock: Block) => {
-        if (isValidnewBlock(newBlock, getLatestBlock())) {
-                blockchain.push(newBlock);
-                return true;
-        }
-        return false;
+    if (isValidnewBlock(newBlock, getLatestBlock())) {
+        blockchain.push(newBlock);
+        return true;
+    }
+    return false;
 };
 
 const replaceChain = (newBlocks: Block[]) => {
-        if (
-                isValidChain(newBlocks) &&
+    if (
+        isValidChain(newBlocks) &&
         getAccumulatedDifficulty(newBlocks) >
             getAccumulatedDifficulty(getBlockchain())
-        ) {
-                console.log(
-                        "Received blockchain is valid. Replacing current blockchain with received blockchain"
-                );
-                blockchain = newBlocks;
-                broadcastLatest();
-        } else {
-                console.log("Received blockchain invalid");
-        }
+    ) {
+        console.log(
+            "Received blockchain is valid. Replacing current blockchain with received blockchain"
+        );
+        blockchain = newBlocks;
+        broadcastLatest();
+    } else {
+        console.log("Received blockchain invalid");
+    }
 };
 
 export {
-        Block,
-        getBlockchain,
-        getLatestBlock,
-        generateNextBlock,
-        isValidBlockStructure,
-        replaceChain,
-        addBlockToChain
+    Block,
+    getBlockchain,
+    getLatestBlock,
+    generateNextBlock,
+    isValidBlockStructure,
+    replaceChain,
+    addBlockToChain
 };
---
+++ src/tx.ts
@@ -0,0 +1,361 @@
+import * as CryptoJS from "crypto-js";
+import * as ecdsa from "elliptic";
+import * as _ from "lodash";
+
+const ec = new ecdsa.ec("secp256k1");
+
+const COINBASE_AMOUNT: number = 50;
+
+class TxOut {
+    public address: string;
+    public amount: number;
+
+    constructor(address: string, amount: number) {
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class TxIn {
+    public txOutId: string;
+    public txOutIndex: number;
+    public signature: string;
+}
+
+class Transaction {
+    public id: string;
+    public txIns: TxIn[];
+    public txOuts: TxOut[];
+}
+
+const getTransactionId = (transaction: Transaction): string => {
+    const txInContent: string = transaction.txIns
+        .map((txIn: TxIn) => txIn.txOutId + txIn.txOutIndex)
+        .reduce((a, b) => a + b, "");
+
+    const txOutContent: string = transaction.txOuts
+        .map((txOut: TxOut) => txOut.address + txOut.amount)
+        .reduce((a, b) => a + b, "");
+
+    return CryptoJS.SHA256(txInContent + txOutContent).toString();
+};
+
+const validateTransaction = (
+    transaction: Transaction,
+    aUnspentTxOuts: UnspentTxOut[]
+): boolean => {
+    if (getTransactionId(transaction) !== transaction.id) {
+        console.log("invalid tx id: " + transaction.id);
+        return false;
+    }
+    const hasValidTxIns: boolean = transaction.txIns
+        .map(txIn => validateTxIn(txIn, transaction, aUnspentTxOuts))
+        .reduce((a, b) => a && b, true);
+    if (!hasValidTxIns) {
+        console.log("some of the txIns are invalid in tx: " + transaction.id);
+        return false;
+    }
+
+    const totalTxInValues: number = transaction.txIns
+        .map(txIn => getTxInAmount(txIn, aUnspentTxOuts))
+        .reduce((a, b) => a + b, 0);
+
+    const totalTxOutValues: number = transaction.txOuts
+        .map(txOut => txOut.amount)
+        .reduce((a, b) => a + b, 0);
+
+    if (totalTxOutValues !== totalTxInValues) {
+        console.log(
+            "totalTxOutValues !== totalTxInValues in tx: " + transaction.id
+        );
+        return false;
+    }
+
+    return true;
+};
+
+const validateBlockTransactions = (
+    aTransactions: Transaction[],
+    aUnspentTxOuts: UnspentTxOut[],
+    blockIndex: number
+): boolean => {
+    const coinbaseTx = aTransactions[0];
+    if (!validateCoinbaseTx(coinbaseTx, blockIndex)) {
+        console.log(
+            "invalid coinbase transaction: " + JSON.stringify(coinbaseTx)
+        );
+        return false;
+    }
+
+    // check for duplication txIns.
+    const txIns: TxIn[] = _(aTransactions)
+        .map(tx => tx.txIns)
+        .flatten()
+        .value();
+    if (hasDuplicates(txIns)) {
+        return false;
+    }
+
+    const normalTransactions: Transaction[] = aTransactions.slice(1);
+    return normalTransactions
+        .map(tx => validateTransaction(tx, aUnspentTxOuts))
+        .reduce((a, b) => a && b, true);
+};
+
+const hasDuplicates = (txIns: TxIn[]): boolean => {
+    const groups = _.countBy(txIns, txIn => txIn.txOutId + txIn.txOutId);
+    return _(groups)
+        .map((value, key) => {
+            if (value > 1) {
+                console.log("duplicate txIn: " + key);
+                return true;
+            } else {
+                return false;
+            }
+        })
+        .includes(true);
+};
+
+class UnspentTxOut {
+    public readonly txOutId: string;
+    public readonly txOutIndex: number;
+    public readonly address: string;
+    public readonly amount: number;
+
+    constructor(
+        txOutId: string,
+        txOutIndex: number,
+        address: string,
+        amount: number
+    ) {
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+let unspentTxOuts: UnspentTxOut[] = [];
+
+const newUnspentTxOuts: UnspentTxOut[] = newTransactions
+    .map(t => {
+        return t.txOuts.map(
+            (txOut, index) =>
+                new UnspentTxOut(t.id, index, txOut.addresss, txOut.amount)
+        );
+    })
+    .reduce((a, b) => a.concat(b), []);
+
+const consumedTxOuts: UnspentTxOut[] = newTransactions
+    .map(t => t.txIns)
+    .reduce((a, b) => a.concent(b), [])
+    .map(txIn => new UnspentTxOut(txIn.TxOutId, txIn.txOutIndex, "", 0));
+
+const resultingUnspentTxOuts = aUnspentTxOuts
+    .filter(
+        uTxO => !findUnspentTxOut(uTxO.txOutId, uTxO.txOutIndex, consumedTxOuts)
+    )
+    .concat(newUnspentTxOuts);
+
+const isValidTxInStructure = (txIn: TxIn): boolean => {
+    if (txIn == null) {
+        console.log("txIn is null");
+        return false;
+    } else if (typeof txIn.signature !== "string") {
+        console.log("invalid signature type in txIn");
+        return false;
+    } else if (typeof txIn.txOutId !== "string") {
+        console.log("invalid txOutId type in txIn");
+        return false;
+    } else if (typeof txIn.txOutIndex !== "number") {
+        console.log("invalid txOutIndex type in txIn");
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isValidTxOutStructure = (txOut: TxOut): boolean => {
+    if (txOut == null) {
+        console.log("txOut is null");
+        return false;
+    } else if (typeof txOut.address !== "string") {
+        console.log("invalid address type in txOut");
+        return false;
+    } else if (!isValidAddress(txOut.address)) {
+        return false;
+    } else if (typeof txOut.amount !== "number") {
+        console.log("invalid amount typ in txOut");
+        return false;
+    } else {
+        return true;
+    }
+};
+
+const isValidTransactionsStructure = (transactions: Transaction[]): boolean => {
+    return transactions
+        .map(isValidTransactionStructure)
+        .reduce((a, b) => a && b, true);
+};
+
+const isValidTransactionStructure = (transaction: Transaction) => {
+    if (typeof Transaction.id !== "string") {
+        console.log("transaction missing");
+        return false;
+    }
+    if (getTransactionId(transaction) !== Transaction.id) {
+        console.log("invalid tx id: " + transaction.id);
+        return false;
+    }
+};
+
+const validateTxIn = (
+    txIn: TxIn,
+    transaction: Transaction,
+    aUnspentTxOuts: UnspentTxOut[]
+): boolean => {
+    const referencedUTxOut: UnspentTxOut = aUnspentTxOuts.find(
+        uTxO => uTxO.txOutId === txIn.txOutId && uTxO.txOutId === txIn.txOutId
+    );
+    if (referencedUTxOut == null) {
+        console.log("referenced txOut not found: " + JSON.stringify(txIn));
+        return false;
+    }
+    const address = referencedUTxOut.address;
+
+    const key = ec.keyFromPublic(address, "hex");
+    return key.verify(transaction.id, txIn.signature);
+};
+
+const getTxInAmount = (txIn: TxIn, aUnspentTxOuts: UnspentTxOut[]): number => {
+    return findUnspentTxOut(txIn.txOutId, txIn.txOutIndex, aUnspentTxOuts)
+        .amount;
+};
+
+const findUnspentTxOut = (
+    transactionId: string,
+    index: number,
+    aUnspentTxOUts: UnspentTxOut[]
+): UnspentTxOut => {
+    return aUnspentTxOUts.find(
+        uTxO => uTxO.txOutId === transactionId && uTxO.txOutIndex === index
+    );
+};
+
+const getCoinbaseTransaction = (
+    address: string,
+    blockIndex: number
+): Transaction => {
+    const t = new Transaction();
+    const txIn: TxIn = new TxIn();
+    txIn.signature = "";
+    txIn.txOutId = "";
+    txIn.txOutIndex = blockIndex;
+
+    t.txIns = [txIn];
+    t.txOuts = [new TxOut(address, COINBASE_AMOUNT)];
+    t.id = getTransactionId(t);
+    return t;
+};
+
+const signTxIn = (
+    transaction: Transaction,
+    txInIndex: number,
+    privateKey: string,
+    aUnspentTxOuts: UnspentTxOut[]
+): string => {
+    const txIn: TxIn = transaction.txIns[txInIndex];
+    const dataToSign = transaction.txIns;
+    const referencedUnspentTxOut: UnspentTxOut = findUnspentTxOut(
+        txIn.txOutId,
+        txIn.txOutIndex,
+        aUnspentTxOuts
+    );
+    const referencedAddress = referencedUnspentTxOut.address;
+    const key = ec.keyFromPrivate(privateKey, "hex");
+    const signature: string = toHexString(key.sign(dataToSign).toDER());
+    return signature;
+};
+
+const updateUnspentTxOuts = (
+    newTransactions: Transaction[],
+    aUnspentTxOuts: UnspentTxOut[]
+): UnspentTxOut[] => {
+    const newUnspentTxOuts: UnspentTxOut[] = newTransactions
+        .map(t => {
+            return t.txOuts.map(
+                (txOut, index) =>
+                    new UnspentTxOut(t.id, index, txOut.address, txOut.amount)
+            );
+        })
+        .reduce((a, b) => a.concat(b), []);
+    const consumedTxOuts: UnspentTxOut[] = newTransactions
+        .map(t => t.txIns)
+        .reduce((a, b) => a.concat(b), [])
+        .map(txIn => new UnspentTxOut(txIn.txOutId, txIn.txOutIndex, "", 0));
+
+    const resultingUnspentTxOuts = aUnspentTxOuts
+        .filter(
+            uTxO =>
+                !findUnspentTxOut(uTxO.txOutId, uTxO.txOutIndex, consumedTxOuts)
+        )
+        .concat(newUnspentTxOuts);
+
+    return resultingUnspentTxOuts;
+};
+
+const processTransactions = (
+    aTransactions: Transaction[],
+    aUnspentTxOuts: UnspentTxOut[],
+    blockIndex: number
+) => {
+    if (!isValidTransactionsStructure(aTransactions)) {
+        return null;
+    }
+
+    if (!validateBlockTransactions(aTransactions, aUnspentTxOuts, blockIndex)) {
+        console.log("invalid block transactions");
+        return null;
+    }
+    return updateUnspentTxOuts(aTransactions, aUnspentTxOuts);
+};
+
+const toHexString = (byteArray): string => {
+    return Array.from(byteArray, (byte: any) => {
+        return ("0" + (byte & 0xff).toString(16)).slice(-2);
+    }).join("");
+};
+
+const getPublicKey = (aPrivateKey: string): string => {
+    return ec
+        .keyFromPrivate(aPrivateKey, "hex")
+        .getPUblic()
+        .encode("hex");
+};
+
+const validateCoinbaseTx = (
+    transaction: Transaction,
+    blockIndex: number
+): boolean => {
+    if (getTransactionId(transaction) !== transaction.id) {
+        console.log("invalid coinbase tx id: " + transaction.id);
+        return false;
+    }
+    if (transaction.txIns.length !== 1) {
+        console.log("one txIn must be specified in th coinbas transaction");
+        return;
+    }
+    if (transaction.txIns[0].txOutIndex !== blockIndex) {
+        console.log("the txIn index in coinbase tx must be the block height");
+        return false;
+    }
+    if (transaction.txOuts.length !== 1) {
+        console.log("invalid number of txOuts in coinbase transaction");
+        return false;
+    }
+    if (transaction.txOuts[0].amount != COINBASE_AMOUNT) {
+        console.log("invalid coinbase amount in coinbase transaction");
+        return false;
+    }
+    return true;
+};
